d26c53d65098143a6f3d669fc35cf9da1425c2b8
gen <gengineer.web@gmail.com>
2019-07-12 12:23:31 +0900

    スキルカードのコンポーネント分割

---
+++ src/components/SkillLists.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <v-card class="pa-3">
+            <p>{{ title }}</p>
+            <p>{{ explain }}</p>
+            <SkillCards :skills="skills"/>
+        </v-card>
+    </div>
+</template>
+
+<script>
+  import SkillCards from "@/components/SkillCards"
+
+  export default {
+    name: "SkillLists",
+    components: {
+      SkillCards
+    },
+    props: {
+      skills: {
+        type: Object
+      },
+      title: {
+        type: String
+      },
+      explain: {
+        type: String
+      }
+    },
+    data: () => {
+      return {
+        skills: this.skills,
+        title: this.title,
+        explain: this.explain
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Skills.vue
+++ src/views/Skills.vue
@@ -1,16 +1,20 @@
 <template>
     <section class="skills">
         <v-layout justify-center>
-            <h1>This is my skills page</h1>
+            <h2>Skill Set</h2>
         </v-layout>
 
+        <SkillLists :explain="explainMarkUp" :title="titleMarkUp" :skills="skillsMarkUp" class="skillLists"/>
 
-        <SkillCards :skills="markUp"/>
-        <SkillCards :skills="frontEnd"/>
-        <SkillCards :skills="backEnd"/>
-        <SkillCards :skills="database"/>
-        <SkillCards :skills="infrastructure"/>
-        <SkillCards :skills="tools"/>
+        <SkillLists :explain="explainFrontEnd" :title="titleFrontEnd" :skills="skillsFrontEnd" class="skillLists"/>
+
+        <SkillLists :explain="explainBackEnd" :title="titleBackEnd" :skills="skillsBackEnd" class="skillLists"/>
+
+        <SkillLists :explain="explainDatabase" :title="titleDatabase" :skills="skillsDatabase" class="skillLists"/>
+
+        <SkillLists :explain="explainInfra" :title="titleInfra" :skills="skillsInfra" class="skillLists"/>
+
+        <SkillLists :explain="explainTools" :title="titleTools" :skills="skillsTools" class="skillLists"/>
 
         <v-layout row wrap>
             <v-flex v-for="(license, index) in licenses" :key="index" xs12 md6>
@@ -25,43 +29,62 @@
 </template>
 
 <script>
-  import SkillCards from "@/components/SkillCards"
+  import SkillLists from "@/components/SkillLists"
 
   export default {
     name: "Skills",
     components: {
-      SkillCards
+      SkillLists
     },
     data: () => {
       return {
-        markUp: [
+        titleMarkUp: "マークアップ",
+        explainMarkUp: "マークアップ言語です",
+        skillsMarkUp: [
           { name: 'HTML', years: 4, rate: 4 },
           { name: 'CSS', years: 4, rate: 4 },
           { name: 'SCSS', years: 0, rate: 2 }
         ],
-        frontEnd: [
+
+
+        titleFrontEnd: "フロントエンド",
+        explainFrontEnd: "フロントエンドです",
+        skillsFrontEnd: [
           { name: 'JavaScript', years: 1, rate: 2 },
           { name: 'Vue.js', years: 0, rate: 2 }
         ],
-        backEnd: [
+
+        titleBackEnd: "バックエンド",
+        explainBackEnd: "バックエンドです",
+        skillsBackEnd: [
           { name: 'Java', years: 3, rate: 3 },
           { name: 'ESQL', years: 2, rate: 3 },
           { name: 'Bash', years: 0.5, rate: 2 },
           { name: 'Python', years: 0, rate: 1.5 }
         ],
-        database: [
+
+        titleDatabase: "データベース",
+        explainDatabase: "データベースです",
+        skillsDatabase: [
           { name: 'Oracle', years: 4.5, rate: 3.5 },
           { name: 'MySQL', years: 0, rate: 1.5 }
         ],
-        infrastructure: [
+
+        titleInfra: "インフラ",
+        explainInfra: "インフラです",
+        skillsInfra: [
           { name: 'GCP', years: 0, rate: 1 }
         ],
-        tools: [
+
+        titleTools: "ツール",
+        explainTools: "ツールです",
+        skillsTools: [
           { name: 'FireWorks', years: 2.5, rate: 3 },
           { name: 'subversion', years: 4, rate: 3 },
           { name: 'Git', years: 0, rate: 1 },
           { name: 'Excel/VBA', years: 5, rate: 4 }
         ],
+
         licenses: [
           { name: '基本情報技術者', year: 2015 },
           { name: 'TOEIC 790点', year: 2019 }
@@ -72,5 +95,7 @@
 </script>
 
 <style lang="scss" scoped>
-
+.skillLists{
+    margin: 10px;
+}
 </style>

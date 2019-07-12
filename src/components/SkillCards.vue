886ae21288f2ef0838eb6c43211aea0cdab62a35
gen <gengineer.web@gmail.com>
2019-07-12 11:06:26 +0900

    スキルカードのリファクタリング

---
+++ src/components/SkillCards.vue
@@ -0,0 +1,43 @@
+<template>
+    <v-layout row wrap>
+        <v-flex v-for="(skill, index) in skills" :key="index" xs12 md6>
+            <v-card class="ma-3 pa-3">
+                <h3>{{ skill.name }}</h3>
+                <v-divider/>
+                <p>実務経験年数:
+                    <span v-if="skill.years === 0"> 数ヶ月(独学)</span>
+                    <span v-else-if="skill.years === 0.5"> 半年</span>
+                    <span v-else> {{ skill.years }} 年</span>
+                </p>
+                <v-rating
+                        v-model="skill.rate"
+                        color="blue darken-4"
+                        background-color="red darken-2"
+                        half-icon="$vuetify.icons.ratingHalf"
+                        half-increments
+                        readonly
+                ></v-rating>
+            </v-card>
+        </v-flex>
+    </v-layout>
+</template>
+
+<script>
+  export default {
+    name: "SkillCards",
+    props: {
+      skills: {
+        type: Object,
+      }
+    },
+    data: () => {
+      return {
+        skills: this.skills
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>

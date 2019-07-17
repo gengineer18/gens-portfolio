d9f8f52df41ce538baf819e322ef2218612dcf59
gen <gengineer.web@gmail.com>
2019-07-17 23:46:18 +0900

    firebaseの導入

--- src/main.js
+++ src/main.js
@@ -3,8 +3,21 @@ import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCfD3sDknwVTPTSgXGqLeV4dPuIl9rUWF8",
+  authDomain: "gens-portfolio.firebaseapp.com",
+  databaseURL: "https://gens-portfolio.firebaseio.com",
+  projectId: "gens-portfolio",
+  storageBucket: "",
+  messagingSenderId: "926726598258",
+  appId: "1:926726598258:web:8403f0e3cb1b1f8d"
+};
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,

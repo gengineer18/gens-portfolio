460c8b7b17ddac4b1508ce9c772cc5567bc952f8
gen <gengineer.web@gmail.com>
2019-07-21 17:42:02 +0900

    サイドナビゲーションの追加

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <template>
   <v-app>
     <v-toolbar app>
-      <v-toolbar-side-icon></v-toolbar-side-icon>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Gen's Portfolio Site</span>
+      <v-toolbar-side-icon @click.stop="OpenSideMenu"/>
+      <v-toolbar-title class="headline">
+        <span>Gen Senoo's Portfolio Site</span>
       </v-toolbar-title>
-      <v-spacer></v-spacer>
     </v-toolbar>
 
     <v-content>
@@ -14,24 +13,32 @@
       </v-layout>
       <router-view/>
     </v-content>
+    <SideNav/>
   </v-app>
 </template>
 
 <script>
-import 'normalize.css'
-import MyHeader from "@/components/MyHeader";
+  import 'normalize.css'
+  import MyHeader from "@/components/MyHeader";
+  import SideNav from "@/components/SideNav";
 
-export default {
-  name: 'App',
-  components: {
-    MyHeader
-  },
-  data () {
-    return {
-      //
+  export default {
+    name: 'App',
+    components: {
+      MyHeader,
+      SideNav,
+    },
+    data() {
+      return {
+        //
+      }
+    },
+    methods: {
+      OpenSideMenu() {
+        this.$store.dispatch('toggleSideMenu')
+      }
     }
   }
-}
 </script>
 
 <style lang="scss">
---
+++ src/components/SideNav.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-layout wrap style="height: 200px;">
+    <v-navigation-drawer v-model="$store.state.drawer" absolute temporary>
+
+      <v-list class="pt-0">
+        <v-list-tile v-for="item in items"
+                     :key="item.title"
+                     :to='item.link'>
+          <v-list-tile-action>
+            <v-icon color="blue darken-4">{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+
+          <v-list-tile-content>
+            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+  </v-layout>
+</template>
+
+<script>
+  export default {
+    name: "SideNav",
+    data: () => {
+      return {
+        items: [
+          {title: 'Home', icon: 'home', link: '/'},
+          {title: 'About Me', icon: 'account_box', link: 'about'},
+          {title: 'Skill Set', icon: 'laptop_mac', link: 'skills'},
+          {title: 'My Works', icon: 'work', link: 'works'},
+          {title: 'Contact Me', icon: 'message', link: 'contact'},
+        ]
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
